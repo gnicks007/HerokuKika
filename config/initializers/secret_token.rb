c89544b3abb5aef139d1a1ccf8866f6941f58ed9
gnicks007 <nick.litombe@gmail.com>
2016-09-29 23:05:16 -0400

    Add files via upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KikaMobile1::Application.config.secret_key_base = 'f7955d0c6bf17c092c2173a0507d9174400ad6ac5e1de1609778ba855e0a2ea91ffc5d2a960df7d335416f511ff521d0207cd1dee6595c7fae4ce19a60d0d7f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KikaMobile1::Application.config.session_store :cookie_store, key: '_KikaMobile1_session'
